a326bce28ed63898f31ad33c8c78314b755e89b1
Hader <hader.araujo@gmail.com>
2022-04-21 13:06:11 -0300

    TEMP

--- main.py
+++ main.py
@@ -188,7 +188,7 @@ def _get_next_sub_account_index(actual_sub_account):
 
         max_energy = max({int(sub_account_object['energy_pega_1'] if sub_account_object['energy_pega_1'] != '' else 0), int(sub_account_object['energy_pega_2'] if sub_account_object['energy_pega_2'] != '' else 0), int(sub_account_object['energy_pega_3'] if sub_account_object['energy_pega_3'] != '' else 0)})
 
-        if max_energy != "" and int(max_energy) > 10:
+        if max_energy != "" and int(max_energy) > 3:
             return sub_account_object['sub_account_index']
         else:
             return min(sub_accounts, key=lambda x: x['started_time'])['sub_account_index']
